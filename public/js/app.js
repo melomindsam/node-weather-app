096c694fad93242f7606b09c399ef306917e26cf
Moustapha SAMAKE <moustaphasamake@gmail.com>
2020-05-08 17:33:12 +0200

    Remote previous search result when user clicks on search

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,8 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = searchInput.value;
   messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+  messageThree.textContent = '';
   fetch(`/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
